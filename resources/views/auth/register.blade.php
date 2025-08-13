8a1d6f5fbde0a1ed3a65beb217d4cb49054ce2d8
Jay Long <jay@cyberworldbuilders.com>
2025-08-13 00:32:12 -0500

    user industry integration (#26)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,30 +1,58 @@
-<form method="POST" action="register">
-    {!! csrf_field() !!}
+@extends('app')
 
-    <!-- Errors -->
-    @include('errors.list')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+                <div class="panel-body">
+                    <form method="POST" action="register">
+                        {!! csrf_field() !!}
 
-    <div>
-        Name
-        <input type="text" name="name" value="{{ old('name') }}">
+                        <!-- Errors -->
+                        @include('errors.list')
+
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" value="{{ old('name') }}" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" value="{{ old('email') }}" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="industry_id">Industry</label>
+                            <select name="industry_id" class="form-control">
+                                <option value="">Select Industry</option>
+                                @foreach(App\Industry::orderBy('name')->get() as $industry)
+                                    <option value="{{ $industry->id }}" 
+                                            {{ old('industry_id') == $industry->id ? 'selected' : '' }}>
+                                        {{ $industry->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">Confirm Password</label>
+                            <input type="password" name="password_confirmation" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
-
-    <div>
-        Password
-        <input type="password" name="password">
-    </div>
-
-    <div>
-        Confirm Password
-        <input type="password" name="password_confirmation">
-    </div>
-
-    <div>
-        <button type="submit">Register</button>
-    </div>
-</form>
+</div>
+@endsection
