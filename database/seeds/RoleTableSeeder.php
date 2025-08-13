106eb12b7929402117dc486b6ebf0f22af0cff5b
Jay <jay@cyberworldbuilders.com>
2025-08-13 02:06:58 +0000

    fixing role table seeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class RoleTableSeeder extends Seeder
 {
@@ -14,22 +15,30 @@ class RoleTableSeeder extends Seeder
 		DB::table('roles')->insert([
 			'name' => 'AOE Admin',
 			'slug' => 'admin',
-			'level' => 4
+			'level' => 4,
+			'created_at' => Carbon::now(),
+			'updated_at' => Carbon::now()
 		]);
 		DB::table('roles')->insert([
 			'name' => 'Reseller',
 			'slug' => 'reseller',
-			'level' => 3
+			'level' => 3,
+			'created_at' => Carbon::now(),
+			'updated_at' => Carbon::now()
 		]);
 		DB::table('roles')->insert([
 			'name' => 'Client Admin',
 			'slug' => 'client',
-			'level' => 2
+			'level' => 2,
+			'created_at' => Carbon::now(),
+			'updated_at' => Carbon::now()
 		]);
 		DB::table('roles')->insert([
 			'name' => 'User',
 			'slug' => 'user',
-			'level' => 1
+			'level' => 1,
+			'created_at' => Carbon::now(),
+			'updated_at' => Carbon::now()
 		]);
     }
 }
