bd00996a46ed354c56eac9535419416f826db786
Jay <jay@cyberworldbuilders.com>
2025-08-13 06:27:59 +0000

    good grief

--- config/database.php
+++ config/database.php
@@ -64,7 +64,7 @@ return [
 
         'sqlite' => [
             'driver'   => 'sqlite',
-            'database' => storage_path('database.sqlite'),
+            'database' => env('DB_DATABASE', storage_path('database.sqlite')),
             'prefix'   => '',
         ],
 
