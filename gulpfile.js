a41e3edc8e41814bb7536029e26e950e50cdb583
Jay <jay@cyberworldbuilders.com>
2025-09-22 00:35:08 +0000

    Fix datepicker JavaScript not being copied by Gulp
    - Added datepicker directory copy to gulpfile.js
    - This ensures bootstrap-datepicker.js is available in production
    - Fixes calendar popup not working on staging/production

--- gulpfile.js
+++ gulpfile.js
@@ -67,5 +67,6 @@ elixir(function(mix) {
         .copy('resources/assets/xenon/js/select2', 'public/assets/js/select2')
         .copy('resources/assets/xenon/js/jquery-ui', 'public/assets/js/jquery-ui')
         .copy('resources/assets/xenon/js/selectboxit', 'public/assets/js/selectboxit')
+        .copy('resources/assets/xenon/js/datepicker', 'public/assets/js/datepicker')
         .copy('resources/assets/xenon/css/fonts', 'public/assets/css/fonts');
 });
