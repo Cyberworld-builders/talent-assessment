299371e9761625a8dbaea22e1b347f8b195eb799
Jay <jay@cyberworldbuilders.com>
2025-10-20 17:17:05 +0000

    Add basic HTML formatting to reminder email
    Added minimal HTML tags to match assignment.blade.php format:
    - <p> tags for paragraphs
    - <br/> tags for line breaks
    - <a> tag for login link
    - <div> for footer
    - No CSS or inline styling
    
    This ensures line breaks render properly in email clients while
    maintaining the simple, clean aesthetic of notification emails.

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,25 +1,29 @@
-Hello, {{ $user->name }}
+<p>Hello, {{ $user->name }}</p>
 
-This is a friendly reminder that you have a pending assessment that needs to be completed.
+<p>
+    This is a friendly reminder that you have a pending assessment that needs to be completed.
+</p>
 
-Assessment: {{ $assessment->name }}
-Expires: {{ $expires->format('l, F jS, Y') }}
-@if($days_remaining > 1)
-Time Remaining: {{ $days_remaining }} days
-@elseif($days_remaining == 1)
-Time Remaining: 1 day
-@elseif($days_remaining == 0)
-⚠️ This assessment expires today!
-@else
-⚠️ This assessment is overdue!
-@endif
+<p>
+    Assessment: {{ $assessment->name }}<br/>
+    Expires: {{ $expires->format('l, F jS, Y') }}<br/>
+    @if($days_remaining > 1)
+        Time Remaining: {{ $days_remaining }} days
+    @elseif($days_remaining == 1)
+        Time Remaining: 1 day
+    @elseif($days_remaining == 0)
+        ⚠️ This assessment expires today!
+    @else
+        ⚠️ This assessment is overdue!
+    @endif
+</p>
 
-Login here to complete your assessment: {{ $assignments_link }}
+<p>
+    Login <a target="_blank" href="{{ $assignments_link }}">here</a> to complete your assessment. You can use the following credentials:<br/>
+    username: {{ $user->username }}<br/>
+    password: {{ $user->generate_password_for_user() }}
+</p>
 
-You can use the following credentials:
-username: {{ $user->username }}
-password: {{ $user->generate_password_for_user() }}
-
-
-© {{ date('Y') }} Involved Talent
+<br/>
+<div class="footer-text">&copy; {{ date('Y') }} Involved Talent</div>
 
